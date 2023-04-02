ef955630890a6aae3b186a4425b1a3388d5cf005
FrancescoAceto <acetof02@gmail.com>
2023-04-02 15:54:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios"
+import Cards from './Cards.vue';
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+  Cards,
+},
+  created() {
+    
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0").then((response) => {
+    console.log(response.data.data);
+  });
+},
+}
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Yu-Gi-Oh! First 50 Card-List</h1>
+    <Cards></Cards>
+  </div>
+</template>
+
+<style>
+ h1{
+  color: white;
+ }
+</style>
---
+++ src/Cards.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'Cards',
+    }
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style>
+
+</style>
